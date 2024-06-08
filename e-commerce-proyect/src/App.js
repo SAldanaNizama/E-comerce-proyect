2a48e1089a58bc6a9308ed655b74128b456bb7c7
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-06-07 20:37:12 -0500

    talwind-implementation

--- e-commerce-proyect/src/App.js
+++ e-commerce-proyect/src/App.js
@@ -1,10 +1,11 @@
+import Example from './components/home/home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>My proyect</h1>
+      <Example/>
       </header>
     </div>
   );
