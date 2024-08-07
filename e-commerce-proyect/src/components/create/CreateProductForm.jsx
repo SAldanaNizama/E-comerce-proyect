9eea0eabad7f3d748051ebbd40af5a7d5904eb82
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-08-07 14:48:01 -0500

    create-form-update-navbar

--- e-commerce-proyect/src/components/create/CreateProductForm.jsx
+++ e-commerce-proyect/src/components/create/CreateProductForm.jsx
@@ -11,6 +11,7 @@ const CreateProductForm = () => {
     subcategoryId: "",
   });
 
+  const [images, setImages] = useState([]);
   const [marcas, setMarcas] = useState([]);
   const [subcategories, setSubcategories] = useState([]);
 
@@ -49,64 +50,106 @@ const CreateProductForm = () => {
     });
   };
 
+  const handleFileChange = (e) => {
+    setImages([...e.target.files]);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", product.name);
+    formData.append("description", product.description);
+    formData.append("price", product.price);
+    formData.append("stock", product.stock);
+    formData.append("marcaId", product.marcaId);
+    formData.append("subcategoryId", product.subcategoryId);
+    images.forEach((image, index) => {
+      formData.append(`images`, image);
+    });
+
     try {
       const response = await axios.post(
-        "https://e-commerce-test-hqul.onrender.com/products",
-        product
+        "https://e-commerce-test-hqul.onrender.com/products/create",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
       );
       console.log(response.data);
+      // Manejar la respuesta del backend
     } catch (error) {
       console.error("Error al crear el producto:", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Nombre:
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg"
+    >
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          Nombre:
+        </label>
         <input
           type="text"
           name="name"
           value={product.name}
           onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
         />
-      </label>
-      <br />
-      <label>
-        Descripción:
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="description"
+        >
+          Descripción:
+        </label>
         <input
           type="text"
           name="description"
           value={product.description}
           onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
         />
-      </label>
-      <br />
-      <label>
-        Precio:
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="price">
+          Precio:
+        </label>
         <input
           type="number"
           name="price"
           value={product.price}
           onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
         />
-      </label>
-      <br />
-      <label>
-        Stock:
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="stock">
+          Stock:
+        </label>
         <input
           type="number"
           name="stock"
           value={product.stock}
           onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
         />
-      </label>
-      <br />
-      <label>
-        Marca:
-        <select name="marcaId" value={product.marcaId} onChange={handleChange}>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="marcaId">
+          Marca:
+        </label>
+        <select
+          name="marcaId"
+          value={product.marcaId}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
+        >
           <option value="">Seleccione una marca</option>
           {marcas.map((marca) => (
             <option key={marca.id} value={marca.id}>
@@ -114,14 +157,19 @@ const CreateProductForm = () => {
             </option>
           ))}
         </select>
-      </label>
-      <br />
-      <label>
-        Subcategoría:
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 font-bold mb-2"
+          htmlFor="subcategoryId"
+        >
+          Subcategoría:
+        </label>
         <select
           name="subcategoryId"
           value={product.subcategoryId}
           onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded"
         >
           <option value="">Seleccione una subcategoría</option>
           {subcategories.map((subcategory) => (
@@ -130,9 +178,25 @@ const CreateProductForm = () => {
             </option>
           ))}
         </select>
-      </label>
-      <br />
-      <button type="submit">Crear Producto</button>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="images">
+          Imágenes:
+        </label>
+        <input
+          type="file"
+          name="images"
+          multiple
+          onChange={handleFileChange}
+          className="w-full p-2 border border-gray-300 rounded"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+      >
+        Crear Producto
+      </button>
     </form>
   );
 };
